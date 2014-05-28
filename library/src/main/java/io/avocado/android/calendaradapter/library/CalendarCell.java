e70888ee5b2bbdb03e7869b994b7699eae0fe0cc
mattlogan <matt.b.logan@gmail.com>
2014-05-28 11:44:05 -0700

    Added calculations to determine rects for different num events

--- library/src/main/java/io/avocado/android/calendaradapter/library/CalendarCell.java
+++ library/src/main/java/io/avocado/android/calendaradapter/library/CalendarCell.java
@@ -19,13 +19,16 @@ public class CalendarCell extends View {
     private Paint mTextPaint;
     private Paint mPlusPaint;
 
-    private ArrayList<Rect> mEventRects = new ArrayList<Rect>();
+    private ArrayList<Rect> mOddNumEventRects = new ArrayList<Rect>();
+    private ArrayList<Rect> mEvenNumEventRects = new ArrayList<Rect>();
+
     private int[] mTextOrigin = new int[2];
     private Rect[] mPlusRects = new Rect[2];
 
     private String mDateText;
 
     private int mNumEvents;
+    private int mNumSquaresToDraw; // Including plus sign
 
     private static final int MAX_EVENTS = 3;
 
@@ -66,6 +69,7 @@ public class CalendarCell extends View {
 
     public void setNumEvents(int numEvents) {
         mNumEvents = numEvents;
+        mNumSquaresToDraw = Math.min(numEvents, MAX_EVENTS);
     }
 
     @Override
@@ -75,47 +79,74 @@ public class CalendarCell extends View {
         mTextOrigin[0] = (int) (w / 2.f);
         mTextOrigin[1] = 50;
 
-        int numEventsToShow = Math.min(mNumEvents, MAX_EVENTS);
+        int size = (int) (.125f * w);
+        int offsetFromBottom = (int) (.3f * h);
+        int y = h - offsetFromBottom - size;
+        int space = size + (int) (size / 2.f); // square width + one space
 
-        if (numEventsToShow > 0) {
+        int maxNumEventsOdd;
+        int maxNumEventsEven;
+        if (MAX_EVENTS % 2 == 0) {
+            maxNumEventsEven = MAX_EVENTS;
+            maxNumEventsOdd = MAX_EVENTS - 1;
+        } else {
+            maxNumEventsOdd = MAX_EVENTS;
+            maxNumEventsEven = MAX_EVENTS - 1;
+        }
 
-            int size = (int) (.125f * w);
+        int oddCenterX = (int) (w / 2.f - size / 2.f);
+        for (int i = 0; i < maxNumEventsOdd; i++) {
 
-            int offsetFromBottom = (int) (.3f * h);
+            if (i == 0) {
+                mOddNumEventRects.add(new Rect(oddCenterX, y, oddCenterX + size, y + size));
 
-            int y = h - offsetFromBottom - size;
+            } else if (i % 2 == 0) {
+                int spacesFromCenter = i / 2;
 
-            int space = (int) (size / 2.f);
+                int leftX = oddCenterX - spacesFromCenter * space;
+                mOddNumEventRects.add(new Rect(leftX, y, leftX + size, y + size));
 
-            int totalEventsWidth = numEventsToShow * size + (numEventsToShow - 1) * space;
-
-            int startX = (int) (w / 2.f - totalEventsWidth / 2.f);
-
-            for (int i = 0; i < numEventsToShow; i++) {
-
-                int x = startX + i * (size + space);
-
-                mEventRects.add(new Rect(x, y, x + size, y + size));
+                int rightX = oddCenterX + spacesFromCenter * space;
+                mOddNumEventRects.add(new Rect(rightX, y, rightX + size, y + size));
             }
         }
 
-        if (numEventsToShow < mNumEvents) {
+        int firstTwoSquaresWidth = space + size;
+        int evenLeftCenterX = (int) (w / 2.f - firstTwoSquaresWidth / 2.f);
+        int evenRightCenterX = evenLeftCenterX + space;
+        for (int i = 0; i < maxNumEventsEven; i++) {
 
-            Rect boundingRect = mEventRects.get(numEventsToShow - 1);
+            if (i == 1) {
+                mEvenNumEventRects.add(new Rect(evenLeftCenterX, y, evenLeftCenterX + size, y + size));
+                mEvenNumEventRects.add(new Rect(evenRightCenterX, y, evenRightCenterX + size, y + size));
+            } else if (i % 2 != 0) {
+                int spacesFromCenter = i / 2;
 
-            int strokeThickness = 4;
+                int leftX = evenLeftCenterX - spacesFromCenter * space;
+                mEvenNumEventRects.add(new Rect(leftX, y, leftX + size, y + size));
 
-            int midX = boundingRect.left + (int) (boundingRect.width() / 2.f);
-            int midY = boundingRect.bottom - (int) (boundingRect.height() / 2.f);
-
-            // horizontal stroke
-            mPlusRects[0] = new Rect(boundingRect.left, midY - strokeThickness / 2,
-                    boundingRect.right, midY + strokeThickness / 2);
-
-            //vertical stroke
-            mPlusRects[1] = new Rect(midX - strokeThickness / 2, boundingRect.top,
-                    midX + strokeThickness / 2, boundingRect.bottom);
+                int rightX = evenRightCenterX + spacesFromCenter * space;
+                mEvenNumEventRects.add(new Rect(rightX, y, rightX + size, y + size));
+            }
         }
+
+        // Plus sign
+
+        Rect boundingPlusRect = MAX_EVENTS % 2 == 0 ? mEvenNumEventRects.get(MAX_EVENTS - 1)
+                : mOddNumEventRects.get(MAX_EVENTS - 1);
+
+        int strokeThickness = 4;
+
+        int midX = boundingPlusRect.left + (int) (boundingPlusRect.width() / 2.f);
+        int midY = boundingPlusRect.bottom - (int) (boundingPlusRect.height() / 2.f);
+
+        // horizontal stroke
+        mPlusRects[0] = new Rect(boundingPlusRect.left, midY - strokeThickness / 2,
+                boundingPlusRect.right, midY + strokeThickness / 2);
+
+        //vertical stroke
+        mPlusRects[1] = new Rect(midX - strokeThickness / 2, boundingPlusRect.top,
+                midX + strokeThickness / 2, boundingPlusRect.bottom);
     }
 
     @Override
@@ -124,15 +155,19 @@ public class CalendarCell extends View {
 
         canvas.drawText(mDateText, mTextOrigin[0], mTextOrigin[1], mTextPaint);
 
-        for (int i = 0; i < mEventRects.size(); i++) {
+        ArrayList<Rect> eventRectsToDraw = mNumSquaresToDraw % 2 == 0 ? mEvenNumEventRects : mOddNumEventRects;
 
-            if (i == mEventRects.size() - 1 && mNumEvents > MAX_EVENTS) {
+        for (int i = 0; i < mNumSquaresToDraw; i++) {
+
+            if (i == mNumSquaresToDraw - 1 && mNumEvents > MAX_EVENTS) {
 
                 canvas.drawRect(mPlusRects[0], mPlusPaint);
                 canvas.drawRect(mPlusRects[1], mPlusPaint);
 
             } else {
-                canvas.drawRect(mEventRects.get(i), mEventPaint);
+
+                canvas.drawRect(eventRectsToDraw.get(i), mEventPaint);
+
             }
         }
     }
