531bc05fa7f0ff3a3dd8ebea2d77209fb06ba748
mattlogan <matt.b.logan@gmail.com>
2014-06-02 13:01:50 -0700

    Removed isSameMonth from CalendarUtils

--- library/src/main/java/io/avocado/android/calendaradapter/library/CalendarUtils.java
+++ library/src/main/java/io/avocado/android/calendaradapter/library/CalendarUtils.java
@@ -55,15 +55,4 @@ public class CalendarUtils {
         lastMonthCal.add(Calendar.MONTH, -1);
         return lastMonthCal.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
-
-    public static boolean isSameMonth(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        cal1.setTime(date1);
-
-        Calendar cal2 = Calendar.getInstance();
-        cal2.setTime(date2);
-
-        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH);
-    }
 }
